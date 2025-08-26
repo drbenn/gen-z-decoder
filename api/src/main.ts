7510ad96eee0a2c4b5dcca2d55e7dee473aef2e1
drbenn <bennett.daniel@gmail.com>
2025-08-25 20:31:53 -0400

    changes to monorepo

---
+++ api/src/main.ts
@@ -0,0 +1,22 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+import { ConfigService } from '@nestjs/config';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+  // do not need to determineEnvFile path as envFilePath is passed to ConfigModule.forRoot() in AppModule.
+  const configService = app.get(ConfigService);
+  const port = configService.get<number>('APP_PORT') || 3000;
+  app.setGlobalPrefix(configService.get('URL_GLOBAL_PREFIX') || 'v1');
+  app.enableCors(),
+  // app.enableCors({
+  //   origin: [
+  //     process.env.FRONTEND_URL
+  //   ],
+  //   methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+  //   credentials: true,
+  //   allowedHeaders: 'Content-Type, Authorization',
+  // });
+  await app.listen(port ?? 3000);
+}
+bootstrap();
